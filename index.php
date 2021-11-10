eddec2afec9ae26e8367b4714d8d9bcbc81fd309
logiconroll <anjali11118@gmail.com>
2021-11-10 21:24:50 +0530

    First commit
    commit

---
+++ index.php
@@ -0,0 +1,89 @@
+<html>
+    <head>
+        <style>
+            body{
+                font-family: 'Libre Baskerville', serif;
+                color: #7d6302;
+            }
+            
+            .a1{
+                text-align: center;
+                width: 66%;
+                margin-left: auto;
+                /*border: 1px solid #00FF00;*/
+                margin-right: auto;
+                vertical-align: top;
+            }
+            .px-17{
+                font-size: 17px;
+            }
+            .px-13{
+                font-size: 13px;
+            }
+            .paris{
+                font-family: 'Parisienne', cursive !important;
+            }
+            .large-golden{
+                font-size: 65px;
+                text-align: center;
+                color: #b8a45d;
+            }
+            .cent{
+                text-align:center;
+            }
+            .mt-10{
+                margin-top:10px;
+            }
+            .gold{
+                color: #b8a45d;
+            }
+            .big{
+                font-size:22px;
+            }
+        </style>
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@1&family=Parisienne&display=swap" rel="stylesheet">
+    </head>
+    <body style="background: linear-gradient(rgba(255,255,255,.4), rgba(255,255,255,.4)),url(&quot;https://i.ibb.co/mJxQTmt/bodybg.png&quot;);">
+        <table style=" margin-left: auto;margin-right: auto;padding-top:100px;padding-bottom:72px">
+            <tbody>
+                <tr>
+                    <td style="background-image: url(&quot;https://i.ibb.co/bK3GcLV/bgimage.png&quot;);vertical-align: top;background-repeat: no-repeat;width: 886px;height: 1280px;text-align:center;line-height: 1.4;border: 1px solid;box-shadow: #7d6302 0px 8px 24px, #7d6302 0px 16px 56px, #7d6302 0px 24px 80px;">
+                        <div>
+                            <img src="https://i.ibb.co/cxHj90b/ganeshji.png" style="width:12%;margin-top:23.5%;border-radius:6px">
+                        </div>
+                        <div class="a1 px-17">
+                            <br><br>
+                            Respected Sir / Madam,<br><br>
+                            It gives us immense pleasure to inform you that by the Grace of Almighty God and blessings of you all, the marriage of our son
+                        </div>
+                        <div class="a1 paris large-golden">
+                            Gagan
+                        </div>
+                        <div class="a1 px-17 cent">
+                           has been fixed with 
+                        </div>
+                        <div class="a1 paris large-golden">
+                            Ayushi
+                        </div>
+                        <div class="a1 px-13 cent mt-10">
+                            (Daughter of Mr. Vijay Bhushan Shrivastava & Mrs. Neelprabha Srivastava)
+                        </div>
+                        <div class="a1 px-17 cent">
+                            <br>
+                            and will take place on <span class="big gold" style="text-decoration:underline">13th December, 2021</span> at<br>Swami Lilashah Bhavan, Pratap Nagar, Jaipur.<br><br>
+                        </div>  
+                        <div class="a1 px-17" style="text-align: justify">
+                            We earnestly request you to kindly grace the occasion by your esteemed presence and bless the wedding couple. The wedding invitation card with programme details will be sent to you very soon.<br><br>
+                                You are further requested to please confirm your arrival & share your travel itinerary at your earliest, to enable us to make your stay over here comfortable.<br><br>
+                        </div>
+                        <div class="a1 px-17" style="text-align: justify;text-align: right; margin-right: 19%;">
+                            With regards,<br><br>
+                            K.B. Mathur,<br>
+                            Sheela Mathur.
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
